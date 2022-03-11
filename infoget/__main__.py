d17aa09713182e16233882fe1a5355661348aec3
EL BOUINBI MOHAMMED <60387957+Bouinbi@users.noreply.github.com>
2022-03-11 22:09:44 +0100

    Update __main__.py
    1--After__Slitine

--- infoget/__main__.py
+++ infoget/__main__.py
@@ -2,32 +2,16 @@
 import click
 
 from infoget.get_info import scrp
-from infoget.get_scan import scn
-from infoget.get_detect import dtc
-
-
-
 
 @click.command()
 
 @click.option('-g','--gathering',help= " : this options is using for gathering information")
 
-@click.option('-s','--scan',help= " : this options is using for scanning ip")
 
-@click.option('-d','--detection',help= " : this options is using for detection vulnerability")
-
-
-def main(gathering,scan,detection):
+def main(gathering):
 
     """ simple tool for gathering information , scanning port and other ... """
 
     if gathering :
         scrp(gathering)
-
-    if scan :
-        scn(scan) 
-
-    if detection :
-        dtc(detection)
-
 
